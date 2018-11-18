82cb8c86f8b2d48871ac438b5ded7cf71287e041
Sim sungyong <ssy332972@gmail.com>
2018-11-19 02:11:35 +0900

    no message

--- app/src/main/java/com/example/hong/alchul/request/eventRequest.java
+++ app/src/main/java/com/example/hong/alchul/request/eventRequest.java
@@ -7,14 +7,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class eventRequest extends StringRequest {
-    final static private String URL = "http://10.0.2.2:8080/test2.php";
+    final static private String URL = "http://10.0.2.2:8080/teamproject/workday.php";
+    final static private String URL1 = "http://10.0.2.2:8080/teamproject/workday_event.php";
     private Map<String, String> parameters;
 
     public eventRequest(String userId, Response.Listener<String> listener) {
         super(Method.POST, URL, listener, null);
-
         parameters = new HashMap<>();
         parameters.put("userId", userId);
+
+    }
+
+    public eventRequest(String userId, String startday, Response.Listener<String> listener) {
+        super(Method.POST, URL1, listener, null);
+        parameters = new HashMap<>();
+        parameters.put("userId", userId);
+        parameters.put("startday", startday);
 
     }
 
