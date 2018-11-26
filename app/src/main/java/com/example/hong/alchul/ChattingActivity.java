ba8adf26e4c0c0db61854615d32c26847c1a2608
hongjoonki <ghdjk2216@naver.com>
2018-11-27 01:57:56 +0900

    no message

--- app/src/main/java/com/example/hong/alchul/ChattingActivity.java
+++ app/src/main/java/com/example/hong/alchul/ChattingActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -18,16 +23,24 @@ public class ChattingActivity extends Activity{
     String userPhoneNum;
     String userStat;
     String storeCode;
+    EditText editText;
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
 
+    ListView listView;
+
     ArrayList<String> array = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatting);
+
+        editText = (EditText)findViewById(R.id.send_txt);
+        array.add("홍준기");
 
         Intent intent = getIntent();
         userId = intent.getStringExtra("UserId");
@@ -36,12 +49,19 @@ public class ChattingActivity extends Activity{
         userStat = intent.getStringExtra("UserStat");
         storeCode = intent.getStringExtra("StoreCode");
 
+        listView = (ListView) findViewById(R.id.listView);
+
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference();
 
-        // 이제 데이터베이스에 올리기 받아오기해야함
-        // send버튼누르면 올리고, 받아오는건 실시간으로 구현!!!!
+        adapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.chat_item, array);
 
 
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                return false;
+            }
+        });
     }
 }
