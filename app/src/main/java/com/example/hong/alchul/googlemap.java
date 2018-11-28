9f2b8a7b3dc2f6d88de28aa0ce1e24843d0a0bd9
Sim sungyong <ssy332972@gmail.com>
2018-11-28 23:51:44 +0900

    no message

--- app/src/main/java/com/example/hong/alchul/googlemap.java
+++ app/src/main/java/com/example/hong/alchul/googlemap.java
@@ -3,15 +3,20 @@ package com.example.hong.alchul;
 import android.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class googlemap extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
 
     @Override
@@ -23,20 +28,44 @@ public class googlemap extends AppCompatActivity implements OnMapReadyCallback {
         MapFragment mapFragment = (MapFragment)fragmentManager
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(googlemap.this);
+
+
     }
+
+
 
     @Override
     public void onMapReady(final GoogleMap map) {
+        mMap = map;
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Log.i("test", "click은됨");
+                MarkerOptions mOption = new MarkerOptions();
+                //title
+                mOption.title("가게");
+                Double latitude = latLng.latitude;
+                Double longitude = latLng.longitude;
+                String lat = latitude.toString();
+                String lon = longitude.toString();
+                Log.i("test좌표",latitude.toString() );
 
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
-        map.addMarker(markerOptions);
+                mOption.snippet(latitude.toString()+","+longitude.toString());
 
-        map.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-        map.animateCamera(CameraUpdateFactory.zoomTo(15));
+                //latlng: 위도 경도쌍
+                mOption.position(new LatLng(latitude,longitude));
+                map.addMarker(mOption);
+
+
+
+            }
+        });
+
+        LatLng seoul = new LatLng(37.5197889, 126.9403083);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(seoul,14));
     }
 }
+
+
+
