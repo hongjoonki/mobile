eb37b09a453473c2d76e22eb1411c7c85dee127f
hongjoonki <ghdjk2216@naver.com>
2018-11-29 04:13:13 +0900

    no message

--- app/src/main/java/com/example/hong/alchul/manager/manager_home.java
+++ app/src/main/java/com/example/hong/alchul/manager/manager_home.java
@@ -12,7 +12,7 @@ import com.example.hong.alchul.parttime.MyFragment3;
 
 public class manager_home extends AppCompatActivity {
 
-    Bundle bundle = new Bundle(5);
+    Bundle bundle = new Bundle(7);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,49 +24,64 @@ public class manager_home extends AppCompatActivity {
         String userPhoneNum = intent.getStringExtra("UserPhoneNum");
         String userStat = intent.getStringExtra("UserStat");
         String storeCode = intent.getStringExtra("StoreCode");
-        String message = "회원정보: " + userStat + "\n안녕하십니까 " + userId + "님";
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();//intent 받아오기
+        String title = intent.getStringExtra("title");
+        String content = intent.getStringExtra("content");
 
-        LinearLayout button1 = (LinearLayout)findViewById(R.id.button1);
-        LinearLayout button2 = (LinearLayout)findViewById(R.id.button2);
+        if (title != null || content != null) {
+            bundle.putString("UserId", userId);
+            bundle.putString("UserName", userName);
+            bundle.putString("UserPhoneNum", userPhoneNum);
+            bundle.putString("UserStat", userStat);
+            bundle.putString("StoreCode", storeCode);
+            bundle.putString("title", title);
+            bundle.putString("content", content);
+            //  if(userStat == "part")
+            MyFragment3 fragment3 = new MyFragment3();
+            fragment3.setArguments(bundle);
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, fragment3).commit();
+        } else {
+            String message = "회원정보: " + userStat + "\n안녕하십니까 " + userId + "님";
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();//intent 받아오기
 
-        bundle.putString("UserId", userId);         //bundle에 정보를 추가한다.
-        bundle.putString("UserName", userName);
-        bundle.putString("UserPhoneNum", userPhoneNum);
-        bundle.putString("UserStat", userStat);
-        bundle.putString("StoreCode", storeCode);
+            LinearLayout button1 = (LinearLayout)findViewById(R.id.button1);
+            LinearLayout button2 = (LinearLayout)findViewById(R.id.button2);
 
-
-        MyFragment3 fragment3 = new MyFragment3();
-        fragment3.setArguments(bundle);
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, fragment3).commit();
-
-
-
-
-
-        button1.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                manager_frag1 fragment1 = new manager_frag1();
-                fragment1.setArguments(bundle);
-                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, fragment1).commit();
-
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                MyFragment3 fragment3 = new MyFragment3();
-                fragment3.setArguments(bundle);
-                getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, fragment3).commit();
-
-            }
-        });
+            bundle.putString("UserId", userId);         //bundle에 정보를 추가한다.
+            bundle.putString("UserName", userName);
+            bundle.putString("UserPhoneNum", userPhoneNum);
+            bundle.putString("UserStat", userStat);
+            bundle.putString("StoreCode", storeCode);
 
 
+            MyFragment3 fragment3 = new MyFragment3();
+            fragment3.setArguments(bundle);
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, fragment3).commit();
+
+
+
+
+
+            button1.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    manager_frag1 fragment1 = new manager_frag1();
+                    fragment1.setArguments(bundle);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, fragment1).commit();
+
+                }
+            });
+
+            button2.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    MyFragment3 fragment3 = new MyFragment3();
+                    fragment3.setArguments(bundle);
+                    getSupportFragmentManager().beginTransaction().replace(R.id.main_frame, fragment3).commit();
+
+                }
+            });
+        }
     }
 }
